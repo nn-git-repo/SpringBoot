57e6da1ba9036f13c8879ac93fb1c04e11ff4f91
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-02 09:21:19 +0530

    Add files via upload

--- SpringBootEmployeeApi/EmployeeController.java
+++ SpringBootEmployeeApi/EmployeeController.java
@@ -1,1 +1,20 @@
+package com.company.employee_api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	 @GetMapping("/employee")
+	    public Map<String, Object> getEmployeeInfo() {
+	        Map<String, Object> employee = new HashMap<>();
+	        employee.put("id", 101);
+	        employee.put("name", "Namratha N");
+	        employee.put("department", "Engineering");
+	        return employee;
+	    }
+}
